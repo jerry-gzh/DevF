c3e80df39bc88494f2925daa4897cca66fd6fef1
Gerardo Andrés Guzmán Hernández <86629201+jerry-gzh@users.noreply.github.com>
2021-08-25 23:57:03 -0500

    nuevo ejercicio

--- 2. Javascript/Ejercicio 8/main.js
+++ 2. Javascript/Ejercicio 8/main.js
@@ -2,7 +2,7 @@
 // Ejercicio 1
 // Escriba un programa JavaScript que acepte dos números enteros y muestre el más grande
 
-var a = parseInt(prompt("Ingrese un número: "));
+/* var a = parseInt(prompt("Ingrese un número: "));
 var b = parseInt(prompt("Ingrese otro número: "));
 function mayor(a,b){
     if (a>b){
@@ -15,14 +15,14 @@ function mayor(a,b){
         console.log("Los valores no son validos")
     }
 }
-mayor(a,b)
+mayor(a,b) */
 
 //Ejercicio 2 
 // Escriba una declaración condicional (if) de JavaScript para encontrar el signo de la multiplicación de tres números. 
 //Muestre un cuadro de alerta con el signo especificado. 
 //Por ejemplo si los tres números son: 1, -3 y 4, el resultado sería “-“
 
-var a = parseInt(prompt("Ingrese un número positivo o negativo: "));
+/* var a = parseInt(prompt("Ingrese un número positivo o negativo: "));
 var b = parseInt(prompt("Ingrese otro número positivo o negativo: "));
 var c = parseInt(prompt("Ingrese otro número positivo o negativo: "));
 var d = 0; 
@@ -33,13 +33,13 @@ if(d>0){
     alert("La operación es -")
 } else{
     alert("La entrada es inválida")
-}
+} */
 
 
 // Ejercicio 3
 // Escribe un programa de JavaScript para encontrar el mayor de cinco números.
 
-var arreglo = [];
+/* var arreglo = [];
 a = 0;
 for(i=1; i<=5; i++){
     arreglo.push(parseInt(prompt("Ingrese un número: ")))
@@ -48,9 +48,57 @@ arreglo.sort(function(a, b) {
     return a - b;
 });
 a = arreglo.pop();
-console.log(a)
+console.log(a) */
 
 //Ejercicio 4
 //Escriba un bucle for JavaScript que iterará de 0 a 15. 
 //Para cada iteración, verificará si el número actual es par o impar. 
 //Imprimir en cada iteración si el número es par o impar.
+
+/* for (i=0; i<=15; i++){
+    if(i%2){
+        console.log(`El número ${i} es impar`)
+    }else{
+        console.log(`El número ${i} es par`)
+    }
+} */
+
+// Ejercicio 5 
+// Escriba un programa de JS que obtenga el promedio de los siguientes estudiantes,
+// después determine el grado que le corresponde a ese promedio. 
+// Por ejemplo si el promedio es 75 entonces el grado es C según la tabla.
+
+/* let nombres = ["David", "Vinoth", "Divya", "Ishitha", "Thomas"]; 
+let cali = [];
+let grado = [];
+for(i=0; i<=4; i++){
+    cali.push(prompt(`Ingrese la calificación de ${nombres[i]}`));
+}
+for(i=0; i<=4; i++){
+    if (cali[i] <= 60){
+        console.log(`El grado de ${nombres[i]} es F `)
+    }else if (cali[i] <= 70){
+        console.log(`El grado de ${nombres[i]} es D `)
+    }else if (cali[i] <= 80){
+        console.log(`El grado de ${nombres[i]} es C `)
+    }else if (cali[i] <= 90){
+        console.log(`El grado de ${nombres[i]} es B `)
+    }else if (cali[i] <= 100){
+        console.log(`El grado de ${nombres[i]} es A `)
+    }else {
+        console.log(`La calificación no es valida `)
+    }
+} */
+
+// Ejercicio 6 
+//Escriba un programa JavaScript que repita los números enteros del 1 al 100. 
+//Pero para múltiplos de tres imprima "Fizz" en lugar del número y para los múltiplos de cincoimprima "Buzz". 
+//Para números que son múltiplos de tres y cinco, escriba "FizzBuzz".
+
+for(i=0; i<=100; i++){
+    if(i%3){
+        console.log("Fizz")
+    }else if(i%5){
+        console.log("Buzz")
+    }
+}
---
+++ 2. Javascript/Ejercicio 9/main.js
@@ -0,0 +1,17 @@
+function fondoVerde()
+{
+    let bodyTag = document.getElementById("cuerpo") /* Se relaciona el id del HTML con variable de JS */
+    bodyTag.style.backgroundColor = "green"; /* Se aplica acción a variable */
+}
+
+function fondoAzul()
+{
+    let bodyTag = document.getElementById("cuerpo")
+    bodyTag.style.backgroundColor = "blue";
+}
+
+function fondoRosa()
+{
+    let bodyTag = document.getElementById("cuerpo")
+    bodyTag.style.backgroundColor = "pink";
+}
